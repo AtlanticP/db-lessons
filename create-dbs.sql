ac9274979915b1b60efebb2a08417d695473d156
Nikita Davydov <ndavidov@telemediagroup.ru>
2024-03-27 17:11:27 +0300

    create employee db with report

--- create-dbs.sql
+++ create-dbs.sql
@@ -1,24 +1,72 @@
-DROP DATABASE IF EXISTS employee;
-CREATE DATABASE employee;
+--DROP DATABASE IF EXISTS employee;
+--CREATE DATABASE employee;
 
+-- table department
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-	departmentID serial NOT NULL PRIMARY KEY,  -- serial -> auto increment
-	name varchar(30)
+  departmentID serial NOT NULL PRIMARY KEY,  -- serial -> auto increment
+  name varchar(30)
 );
 
-DROP TABLE IF EXISTS employee1;
+INSERT INTO department (departmentID, name) 
+VALUES
+  (1, 'Dep_analit'),
+  (2, 'Dep_prog'),
+  (3, 'Dep_admin');
+
+-- table employee
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-	employeeID serial NOT NULL PRIMARY KEY,
-	name1 varchar(80),
-	job varchar(30), 
-	departmentID int NOT NULL, 
-	FOREIGN KEY (departmentID) REFERENCES department(departmentID)
+  employeeID serial NOT NULL PRIMARY KEY,
+  name varchar(80),
+  job varchar(30), 
+  departmentID int NOT NULL, 
+  CONSTRAINT "DEP" 
+    FOREIGN KEY (departmentID) 
+    REFERENCES department(departmentID)
 );
 
-INSERT INTO employee 
+INSERT INTO employee (employeeID, name, job, departmentID)
 VALUES 
-	(1, 'Programmer', 70000, 5000),
-	(1, 'analitic', 110000, 30000),
-	(1, 'Programmer', 74000, 15000),
-	(1, 'Programmer', 69000, 20000);
+  (100, 'Smit N', 'Programmer', 2),
+  (101, 'Stone J.', 'manager', 3),
+  (102, 'Asser M.', 'analitic', 1),
+  (103, 'Wood N.', 'Programmer', 2),
+  (104, 'Thomson L.', 'Programmer', 2);
+  
+-- table employee1
+DROP TABLE IF EXISTS employee1;
+ CREATE TABLE employee1 AS (
+    SELECT * FROM employee
+ );
+ 
+ALTER TABLE employee1 
+    ADD CONSTRAINT "DEP" 
+    FOREIGN KEY (departmentID) 
+    REFERENCES department(departmentID);
+ 
+INSERT INTO employee (name, job, departmentID)
+VALUES 
+  ('Wirt C', 'Programmer', 5);
+
+INSERT INTO department 
+VALUES 
+  (10, 'Test DELETE');
+
+INSERT INTO employee (name, job, departmentID)
+VALUES 
+  ('Wirt C', 'Programmer', 10);
+
+SELECT * FROM department;
+
+
+
+
+
+
+SELECT * FROM employee;
+
+DELETE 
+
+DROP TABLE department;  
+
